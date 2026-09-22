f449ed86db402010abe559627be37f9c010f434f
Benedikts <134172383+SwortechGitHub@users.noreply.github.com>
2026-09-22 12:49:30 +0300

    updated  battery widget

--- eww/scripts/update_gauge.sh
+++ eww/scripts/update_gauge.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-# Volume for test
-RAW_VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ 2>/dev/null | awk '{print $2}')
-PERCENT=${1:-$(awk -v v="${RAW_VOL:-1.0}" 'BEGIN { printf "%.0f", v * 100 }')}
-
 # Battery
-# BAT_PATH=$(ls -d /sys/class/power_supply/BAT* 2>/dev/null | head -n 1)
-# if [ -n "$BAT_PATH" ] && [ -f "$BAT_PATH/capacity" ]; then
-#     PERCENT=${1:-$(cat "$BAT_PATH/capacity")}
-# else
-#     PERCENT=${1:-100}
-# fi
+BAT_PATH=$(ls -d /sys/class/power_supply/BAT* 2>/dev/null | head -n 1)
+if [ -n "$BAT_PATH" ] && [ -f "$BAT_PATH/capacity" ]; then
+    PERCENT=${1:-$(cat "$BAT_PATH/capacity")}
+else
+    PERCENT=${1:-100}
+fi
 
 #Calculate bar offset
 OFFSET=$(awk -v p="$PERCENT" 'BEGIN { printf "%.0f", -1158 + (11.58 * p) }')
 
 # Define gradient stop colors based on percentage thresholds
-if [ "$PERCENT" -le 20 ]; then
-    COLOR_STOP1="#A71919" # Dark Red
-    COLOR_STOP2="#D52929" # Light Red
-elif [ "$PERCENT" -le 70 ]; then
-    COLOR_STOP1="#A77E19" # Dark Yellow/Orange
-    COLOR_STOP2="#D5AA29" # Light Yellow
+if [ "$PERCENT" -le 21.25 ]; then
+    COLOR_STOP1="#890034" # Dark Red
+    COLOR_STOP2="#AE0750" # Light Red
+elif [ "$PERCENT" -le 54.75 ]; then
+    COLOR_STOP1="#939409" # Dark Yellow/Orange
+    COLOR_STOP2="#B4B733" # Light Yellow
 else
     COLOR_STOP1="#65A719" # Dark Green
     COLOR_STOP2="#87D529" # Light Green
 fi
 
-SVG_PATH="/home/swortech/.config/eww/images/life_gauge.svg"
+SVG_PATH="$HOME/.config/eww/images/life_gauge.svg"
 
 if [ -f "$SVG_PATH" ]; then
     sed -i -E "/id=[\"']gauge_clip[\"']/,/<\/clipPath>/ s/(transform=[\"']translate\()[^)]*(\)[\"'])/\1${OFFSET}, 0\2/" "$SVG_PATH"
